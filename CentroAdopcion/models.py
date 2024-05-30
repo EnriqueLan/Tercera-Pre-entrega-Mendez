1bd514c5ad59fdb1a2bd18214ab07c42869ecb7c
Enrique <enriquelan101@gmail.com>
2024-05-30 20:18:21 -0300

    Cree la app y tambien los diferentes models.

---
+++ CentroAdopcion/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class Perros(models.Model):
+
+    Nombre=models.CharField(max_length=40)
+    Raza=models.CharField(max_length=40)
+    Genero=models.CharField(max_length=40)
+    Edad=models.IntegerField()
+
+
+class Gatos(models.Model):
+
+    Nombre=models.CharField(max_length=40)
+    Raza=models.CharField(max_length=40)
+    Genero=models.CharField(max_length=40)
+    Edad=models.IntegerField()
+
+
+class Mascota_Adoptada(models.Model):
+
+    NombrePersona=models.CharField(max_length=40)
+    ApellidoPersona=models.CharField(max_length=40)
+    DireccionPersona=models.CharField(max_length=100)
+    NumeroPersona=models.IntegerField()
+    NombreMascota=models.CharField(max_length=40)
+    EspecieMascota=models.CharField(max_length=40)
